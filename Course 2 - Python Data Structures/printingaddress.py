1d96d77555e6323182f84d3d100844d63fc1a1a0
Faraz Mamaghani <fgmamagh315@gmail.com>
2018-12-23 13:09:33 -0500

    Update printingaddress.py

--- Course 2 - Python Data Structures/printingaddress.py
+++ Course 2 - Python Data Structures/printingaddress.py
@@ -4,10 +4,10 @@
 # You will parse the From line using split() and print out the second word 
 # in the line (i.e. the entire address of the person who sent the message). Then print out a count at the end.
 # Hint: make sure not to include the lines that start with 'From:'.
-
-You can download the sample data at http://www.py4e.com/code3/mbox-short.txt
+# You can download the sample data at http://www.py4e.com/code3/mbox-short.txt
 
 fname = input("Enter file name: ")
+
 if len(fname) < 1 : fname = "mbox-short.txt"
 
 fh = open(fname)
