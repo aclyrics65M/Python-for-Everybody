efe5ca95eaaae0ee27ec17e492894bc3c0524551
Faraz Mamaghani <fgmamagh315@gmail.com>
2018-12-23 10:44:53 -0500

    Update stringslice.py

--- Course 2 - Python Data Structures/stringslice.py
+++ Course 2 - Python Data Structures/stringslice.py
@@ -2,13 +2,9 @@
 # Convert the extracted value to a floating point number and print it out.
 
 text = "X-DSPAM-Confidence:    0.8475";
-
 length = len(text)
-
 number_Location = text.find(" ")
 
 string_segment = text[number_Location: length]
-
 converted_string =  float(string_segment)
-
 print(converted_string)
